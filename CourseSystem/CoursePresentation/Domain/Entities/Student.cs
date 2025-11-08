7ce49da2a6cd8f46ea6e737d24ac128fa029946e
khankishiekhbpa203 <khankishiekh-bpa203@code.edu.az>
2025-11-08 19:47:40 +0400

    init

---
+++ CourseSystem/CoursePresentation/Domain/Entities/Student.cs
@@ -0,0 +1,14 @@
+﻿
+
+using Domain.Common;
+
+namespace Domain.Models
+{
+    public class Student:BaseEntity
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public Group Group { get; set; }
+    }
+}
